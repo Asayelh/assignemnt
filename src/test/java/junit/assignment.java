bcb993b40985275896a4bd98566a538b16ef56e4
Dell <asayelh3@gmail.com>
2024-03-10 14:58:32 +0300

    first

--- src/test/java/junit/assignment.java
+++ src/test/java/junit/assignment.java
@@ -2,8 +2,8 @@ package junit;
 
 public class assignment {
     public static void main(String[] args) {
-        System.out.println("8");
-        System.out.println(3 );
+        System.out.print("8");
+
 
     }
 
