87ddaac7e5941a801b24842099b1bcda8d52f46f
Dell <asayelh3@gmail.com>
2024-03-15 09:21:30 +0300

    First

---
+++ src/test/java/assignment/assig4.java
@@ -0,0 +1,44 @@
+package assignment;
+
+import junit.Tests;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.awt.*;
+
+/*
+   Go to Amazon  https://www.amazon.com/stores/page/CDF3296F-98B9-4A3E-BCA2-4834A6E96166
+   Scroll to the bottom of the page using a robot
+   Click on the "Back to top" web element
+   Click on the Amazon logo at the bottom of the page (in the footer) using JavascriptExecutor
+   Use Actions to type "ClarusWay" in the search box and perform the search
+*/
+public class assig4  extends Tests {
+    @Test
+    public void amazonScrolling() throws AWTException, InterruptedException {
+
+        driver.get("https://www.amazon.com/stores/page/CDF3296F-98B9-4A3E-BCA2-4834A6E96166");
+        Robot robot = new Robot();
+        //Scroll to the bottom of the page using a robot
+        robot.mouseWheel(10);
+        //Click on the "Back to top" web element
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//span[@class='navFooterBackToTopText']")).click();
+        //Click on the Amazon logo at the bottom of the page (in the footer) using JavascriptExecutor
+        driver.findElement(By.xpath("//a[contains(@href,'/?ref_=footer_logo')]")).click();
+
+
+//       Use Actions to type "ClarusWay" in the search box and perform the search
+        Actions actions = new Actions(driver);
+        By search = By.xpath("//input[@id='twotabsearchtextbox']");
+        actions.moveToElement(driver.findElement(search)).click().sendKeys("ClarusWay").perform();
+
+
+
+
+    }
+}
