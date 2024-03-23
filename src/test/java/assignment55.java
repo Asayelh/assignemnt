27d02d58786bbb075ddec7810fb9fc650c4884a3
Dell <asayelh3@gmail.com>
2024-03-23 08:12:58 +0300

    assignment5

--- src/test/java/assignment55.java
+++ src/test/java/assignment55.java
@@ -1,7 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class assignment55 {
     //Go to URL: http://crossbrowsertesting.github.io/
@@ -16,15 +20,37 @@ public class assignment55 {
     //Doing Cross Browser Testing.
     WebDriver driver = new ChromeDriver();
 @Test
-    public void toDoApp(){
+    public void toDoApp() {
     driver.get("http://crossbrowsertesting.github.io/");
     driver.findElement(By.cssSelector("a[href='todo-app.html']")).click();
     driver.findElement(By.xpath("//input[@name='todo-4']")).click();
     driver.findElement(By.xpath("//input[@name='todo-5']")).click();
+    List<WebElement> toDoList = driver.findElements(By.xpath("//span[contains(@class,'done-true')]"));
+    int listSize = toDoList.size();
+    Assert.assertEquals(listSize, 2, "List length is not as expected");
+    String TextToDoList4 = driver.findElement(By.xpath("//input[contains(@name,'todo-4')]")).getText();
+    String TextToDoList5 = driver.findElement(By.xpath("//input[contains(@name,'todo-2')]")).getText();
 
+    toDoList.forEach(item -> {
+        String text = item.getText();
+        if (text.equals(TextToDoList4)) {
 
+            Assert.assertTrue(true, "Text 'to do 4' not found in checked items");
 
+        }
+        if (text.equals(TextToDoList5)) {
+            Assert.assertTrue(true, "Text 'to do 5' not found in checked items");
 
+        }
+    });
+    driver.findElement(By.xpath("//a[@ng-click='todoList.archive()']")).click();
+    List<WebElement> restlist = driver.findElements(By.xpath("//span[@class='done-false']"));
+    int total = restlist.size();
+    Assert.assertEquals(total, 3);
+}
 
     }
-}
+
+
+
+
